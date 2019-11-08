e9d1b12667f3af0841149e55012105146c412a2d
Yan Li <yali@microstrategy.com>
2019-11-08 10:00:08 -0500

    Init Repo

---
+++ WebViewDownloadFromDataURLTest/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WebViewDownloadFromDataURLTest
+//
+//  Created by Li, Yan on 11/8/19.
+//  Copyright © 2019 MicroStrategy, Inc. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WebViewDownloadFromDataURLTest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WebViewDownloadFromDataURLTest
+//
+//  Created by Li, Yan on 11/8/19.
+//  Copyright © 2019 MicroStrategy, Inc. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
